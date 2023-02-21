0365f87d11f906cc73693696ac1ff3154088a5af
NishithaAkepati <nishithaakepati@gmail.com>
2023-02-21 13:55:54 +0530

    Commitfinal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="Vector.svg" width="300" height="300"  className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://www.propelleraero.com/dirtmate/"
+
         >
           Learn React
         </a>
